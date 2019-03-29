b199b8077e99a2aed8fd82f9f2373343b958aca4
Roman <quke@inbox.ru>
2019-03-30 00:04:35 +0400

    maven commit

--- hw01maven/src/main/java/ru/otus/HelloOtus.java
+++ hw01maven/src/main/java/ru/otus/HelloOtus.java
@@ -1,4 +1,10 @@
 package ru.otus;
 
+import com.google.common.base.Splitter;
+
 public class HelloOtus {
+    public static void main(String[] args) {
+        Iterable<String> arr = Splitter.on(',').trimResults().omitEmptyStrings().split("abc, bcd,, cde   ,zsa");
+        System.out.println(arr.toString());
+    }
 }
