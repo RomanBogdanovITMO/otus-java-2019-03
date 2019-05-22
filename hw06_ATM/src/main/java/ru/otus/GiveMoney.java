79a8e07c52cb20de9021bb9079b7a143c6ad3c84
Roman <quke@inbox.ru>
2019-05-22 19:24:27 +0400

    11.04.2019

---
+++ hw06_ATM/src/main/java/ru/otus/GiveMoney.java
@@ -0,0 +1,20 @@
+package ru.otus;
+
+import java.util.Map;
+
+public class GiveMoney implements operation {
+    @Override
+    public void action(String title, int money, Map<Integer,Integer> map) {
+        int count = 0;
+        for (Map.Entry<Integer,Integer> entry: map.entrySet()){
+            if (entry.getKey()== money){
+                count++;
+                System.out.println(title + " " + entry.getKey());
+                int countMapBancknot = entry.getValue() - count;
+                entry.setValue(countMapBancknot);
+
+            }
+        }
+        System.out.println("ATM " + map);
+    }
+}
---
+++ hw06_ATM/src/main/java/ru/otus/operation.java
@@ -0,0 +1,7 @@
+package ru.otus;
+
+import java.util.Map;
+
+public interface operation {
+    void action(String title, int money, Map<Integer,Integer> map);
+}
