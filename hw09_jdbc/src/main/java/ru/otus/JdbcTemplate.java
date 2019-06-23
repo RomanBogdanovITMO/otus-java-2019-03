b943d6bcdc1cc4681ad69fd2b503cd680c147738
Roman <quke@inbox.ru>
2019-06-23 15:12:43 +0400

    23.06.2019

--- hw09_jdbc/src/main/java/ru/otus/JdbcTemplate.java
+++ hw09_jdbc/src/main/java/ru/otus/JdbcTemplate.java
@@ -1,10 +1,17 @@
 package ru.otus;
 
 import ru.otus.dao.MyId;
+import ru.otus.executor.DBExcecutorImpl;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class JdbcTemplate {
     private final Connection connection;
@@ -14,31 +21,103 @@ public class JdbcTemplate {
         this.connection = connection;
     }
 
-    public void create(Object object)  {
-        String param1 = null;
-        String param2 = null;
+    public void create(Object object) throws IllegalAccessException, SQLException {
+        String sqlParam1 = null;
+        String sqlParam2 = null;
         String sqlInsert = null;
+        List<Object> paramObjects = new ArrayList<>();
+
+        DBExcecutorImpl<Object> dbExcecutor = new DBExcecutorImpl<>(connection);
+
         Class<?> clazz = object.getClass();
         Field[] fields = clazz.getDeclaredFields();
-        System.out.println();
+        //System.out.println();
         for (Field fl : fields) {
+            fl.setAccessible(true);
+            if (fl.get(object) instanceof String) {
+                String str = (String) fl.get(object);
+                paramObjects.add(str);
+            } else if (fl.get(object) instanceof Integer) {
+                int parametr1 = (int) fl.get(object);
+                paramObjects.add(parametr1);
+            } else if (fl.get(object) instanceof Long) {
+                long parametr2 = (long) fl.get(object);
+                paramObjects.add(parametr2);
+            }
             Annotation[] annotations = fl.getDeclaredAnnotations();
-            System.out.println(fl.getName());
+            // System.out.println(fl.getName());
             for (Annotation an : annotations) {
                 if (an instanceof MyId) {
-                    for (Field fl1 : fields){
-                        if (param1 == null){
-                            param1 = fl1.getName();
-                        }else if (param2 == null){
-                            param2 = fl1.getName();
+                    for (Field fl1 : fields) {
+                        if (sqlParam1 == null) {
+                            sqlParam1 = fl1.getName();
+                        } else if (sqlParam2 == null) {
+                            sqlParam2 = fl1.getName();
                         }
                     }
-                     sqlInsert = "insert into " + clazz.getSimpleName().toLowerCase() +
-                             "(" + param1.toLowerCase() + "," + param2.toLowerCase() + ") " + "values (?,?)";
-
+                    sqlInsert = "insert into " + clazz.getSimpleName().toLowerCase() +
+                            "(" + sqlParam1.toLowerCase() + "," + sqlParam2.toLowerCase() + ") " + "values (?,?)";
                 }
             }
         }
+        System.out.println("before " + paramObjects);
+        long id = dbExcecutor.created(sqlInsert, paramObjects);
+        for (Field fl : fields) {
+            fl.setAccessible(true);
+            if (fl.get(object) instanceof Long) {
+                System.out.println("bedore " + fl.get(object));
+                fl.set(object, id);
+                long parametr2 = (long) fl.get(object);
+                System.out.println("after " + fl.get(object));
+            }
+
+       /* Method method = clazz.getMethod("getAge");
+        if (method.invoke(object)instanceof String){
+
+            System.out.println("yess");
+        }*/
+
+
+        }
+        System.out.println(id);
         System.out.println(sqlInsert);
+    }
+
+    public void load(Object object,long id) throws NoSuchMethodException {
+        String sqlParamId = null;
+        String sqlParam2 = null;
+        String sqlParam3 = null;
+        String sqlSelect = null;
+
+        DBExcecutorImpl<Object> dbExcecutor = new DBExcecutorImpl<>(connection);
+
+        Class<?> clazz = object.getClass();
+        Method method = clazz.getMethod("getAge");
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field: fields){
+            if ((sqlParamId == null) & (field.getName().toLowerCase().equals("id"))) {
+                sqlParamId = field.getName();
+            } else if (sqlParam2 == null) {
+                sqlParam2 = field.getName();
+            }else if (sqlParam3 == null){
+                sqlParam3 = field.getName();
+            }
+        }
+        sqlSelect = "select " + sqlParamId.toLowerCase() + ", " + sqlParam2.toLowerCase() + ", " +
+                sqlParam3.toLowerCase()+ " from " + clazz.getSimpleName().toLowerCase() +
+                " where " + sqlParamId.toLowerCase() + " = ?";
+        //как загрузить обьект из бд. если мы не знаем какой обьект будет на входе
+       /* Optional<Object> obj = dbExcecutor.load(sqlSelect, id, (ResultSet resultSet) -> {
+            try {
+                if (resultSet.next()) {
+                    return new Object(resultSet);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return null;
+        });*/
+        System.out.println(sqlSelect);
+
     }
 }
