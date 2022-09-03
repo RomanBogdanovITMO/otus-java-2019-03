50b06fea5fb0ecd31b466ccead3128fee9ff6d54
RomanBogdanovITMO <quke@inbox.ru>
2022-09-03 13:59:34 +0300

    updated refactoring

--- hw03_testfram/src/main/java/ru/otus/Main.java
+++ hw03_testfram/src/main/java/ru/otus/Main.java
@@ -1,17 +1,22 @@
 package ru.otus;
 
+import java.util.logging.Logger;
+
 public class Main {
+
+    static Logger logger = Logger.getLogger(TestRunner.class.getName());
+
     public static void main(String[] args) {
         startTest("ru.otus.test.FirstTest");
 
     }
-    public static void startTest(String className){
-        System.out.println("Testing class: " + className);
+
+    public static void startTest(String className) {
+        logger.info("Testing class: " + className);
         try {
             TestRunner.runTests(className);
-        } catch (ClassNotFoundException e){
-            System.out.println("Error: Class not found");
-            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            logger.warning("Error: Class not found: " + e.getMessage());
         }
     }
 }
